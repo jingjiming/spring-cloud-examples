d70fb89d31c20627d8f2d6b9c30fb10f163755f9
jingjiming <1102200163@qq.com>
2020-05-22 17:39:29 +0800

    + config repo

---
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/alpha/config/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.alpha.config.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController {
+
+    @Value("${alpha.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
